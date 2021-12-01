5f806b6f86080927aeb65b7aa486ef36bcd93f6a
jonosmith <jonathansmith@hotmail.com.au>
2021-12-01 22:14:55 +1100

    Adding icons to the terminal

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -1,7 +1,7 @@
-alias ls="exa"
-alias ll="exa -al"
-alias lt="exa -alT"
-alias ltl="exa -alT -L"
+alias ls="exa --icons"
+alias ll="exa --icons -al"
+alias lt="exa --icons -alT"
+alias ltl="exa --icons -alT -L"
 alias git-tree="git log --all --graph --decorate --oneline --simplify-by-decoration"
 alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
 
