aac2f2c4c4c32685d63687f94d26d8ea386659ce
Jonathan Smith <jonathan.smith@versent.com.au>
2021-12-03 17:46:00 +1100

    Renaming an alias

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -2,7 +2,7 @@ alias ls="exa --icons"
 alias ll="exa --icons -al"
 alias lt="exa --icons -alT"
 alias ltl="exa --icons -alT -L"
-alias git-tree="git log --all --graph --decorate --oneline --simplify-by-decoration"
+alias gt="git log --all --graph --decorate --oneline --simplify-by-decoration"
 alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
 
 # XCode
