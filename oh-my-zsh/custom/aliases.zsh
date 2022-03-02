fb927ab08ecb239275814cffdf1de73fa9dfa80e
Jonathan Smith <jonathansmith@hotmail.com.au>
2022-03-03 09:27:44 +1100

    Adding alias for vagrant ssh

--- oh-my-zsh/custom/aliases.zsh
+++ oh-my-zsh/custom/aliases.zsh
@@ -4,6 +4,7 @@ alias lt="exa --icons -alT"
 alias ltl="exa --icons -alT -L"
 alias gt="git log --all --graph --decorate --oneline --simplify-by-decoration"
 alias code="/Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code"
+alias vagrant-ssh="TERM=xterm-256color vagrant ssh" # Fix for Kitty
 
 # XCode
 alias xcrun-booted="xcrun simctl list | egrep '(Booted)'"
