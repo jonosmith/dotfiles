279660873b95777eb07336215ee82afc435cfc94
Jonathan Smith <jonathansmith@hotmail.com.au>
2023-05-02 15:14:56 +1000

    Updating install script

--- install.sh
+++ install.sh
@@ -4,58 +4,89 @@
 
 if [[ $(command -v brew) == "" ]]; then
     echo "Installing Hombrew"
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     # Install Homebrew Cask
     brew tap caskroom/cask
+
+    # Add brew command to PATH
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/jonathansmith/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
 else
     echo "Updating Homebrew"
     brew update
 fi
 
+# Install Rosetta for Apple Silicon Macs
+if [[ $(uname -m) == 'arm64' ]]; then
+    softwareupdate --install-rosetta --agree-to-license
+fi
+
+# Ensure Command Line Tools is up to date
+softwareupdate --all --install --force
+
 echo -n "Install all base packages (Y/n) => "; read answer
 if [[ $answer != "n" ]] && [[ $answer != "N" ]] ; then
     brew install \
+        google-chrome \
+        istat-menus \
         mas \
         exa \
         git \
-        node \
-        deno \
         watchman \
         kitty \
-        dash \
         slack \
-        postman \
+        notion \
+        intellij-idea \
         visual-studio-code \
         cleanshot \
         awscli \
-        docker \
         private-internet-access \
         istat-menus \
         calibre \
         dictionaries \
         daisydisk \
-        transmission \
         heroku/brew/heroku \
-        postgresql \
         webp \
-        jq
+        jq \
+        vlc
 
-    brew install --head pyenv
+    brew install --cask \
+      transmission \
+      dash \
+      dictionaries
     
     mas install 441258766       # Magnet
     mas install 1440147259      # Adguard for Safari
+fi
 
-    # Neovim
+echo -n "Install programming language packages (Y/n) => "; read answer
+if [[ $answer != "n" ]] && [[ $answer != "N" ]] ; then
+    # Node
+    brew install \
+        node \
+        yarn
+    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+    # Python
+    brew install --head pyenv
+    pyenv install $(pyenv install --list | grep --extended-regexp "^\s*[0-9][0-9.]*[0-9]\s*$" | tail -1)
+
+    # React Native
+    brew install --cask react-native-debugger
 fi
 
 echo -n "Install Vim packages? (Y/n) => "; read answer
 if [[ $answer != "n" ]] && [[ $answer != "N" ]] ; then
     brew install neovim
+    brew install rippgrep # For Telescope grep searching
     brew install gnu-sed  # For nvim-spectre
     pip3 install --upgrade pynvim # For packages that rely on python (vim-mundo)
     git clone --depth 1 https://github.com/wbthomason/packer.nvim \
        ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+
+    # Dependencies for neovim languages
+    brew install stylua
 fi
 
 echo -n "Install work apps? (Y/n) => "; read answer
@@ -63,12 +94,16 @@ if [[ $answer != "n" ]] && [[ $answer != "N" ]] ; then
     brew install \
         microsoft-office \
         microsoft-teams \
-        zoom
+        1password \
+        zoom \
+        box-sync \
+        meetingbar
 fi
 
 echo -n "Install terminal packages (Y/n) => "; read answer
 if [[ $answer != "n" ]] && [[ $answer != "N" ]] ; then
     # ZSH
+    brew install zsh
     sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
     # ZSH plugins
--- macosdefaults.sh
+++ macosdefaults.sh
@@ -6,4 +6,7 @@ defaults write com.apple.finder AppleShowAllFiles -bool true
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
 
+# Disable press and hold
+defaults write -g ApplePressAndHoldEnabled -bool false
+
 echo "macOS preferences set"
