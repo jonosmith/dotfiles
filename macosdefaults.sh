f5b7d9d89e73ed7937eaa71620b8127634b40e0c
Jonathan Smith <jonathansmith@hotmail.com.au>
2023-08-06 13:57:10 +1000

    Removing setting to auto hide the macos dock

--- macosdefaults.sh
+++ macosdefaults.sh
@@ -3,9 +3,6 @@
 # Finder: show hidden files by default
 defaults write com.apple.finder AppleShowAllFiles -bool true
 
-# Automatically hide and show the Dock
-defaults write com.apple.dock autohide -bool true
-
 # Disable press and hold
 defaults write -g ApplePressAndHoldEnabled -bool false
 
